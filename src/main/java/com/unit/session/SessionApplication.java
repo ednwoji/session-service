75c359750379c9b952b68eb11c1c296595fcccfe
Emeka Nwoji <ednwoji@unionbankng.com>
2023-12-20 21:24:46 +0100

    Modified the code to work for mocha

--- src/main/java/com/unit/session/SessionApplication.java
+++ src/main/java/com/unit/session/SessionApplication.java
@@ -1,10 +1,20 @@
 package com.unit.session;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SessionApplication {
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
+public class SessionApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SessionApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SessionApplication.class, args);
